cb3171711e7fae5e5819e6f0bc1c0d68eba24570
Antonio Felipe Melo Silva <antonio.melo@stone.com.br>
2022-03-15 21:30:53 -0300

    add boilerplate service

--- src/CapitalGainCli.Domain/Entities/TaxCalculationResult.cs
+++ src/CapitalGainCli.Domain/Entities/TaxCalculationResult.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CapitalGainCli.Domain.DTOs
+namespace CapitalGainCli.Domain.Entities
 {
     public class TaxCalculationResult
     {
---
+++ src/CapitalGainCli.Domain/Services/TaxCalculationService.cs
@@ -0,0 +1,15 @@
+﻿using CapitalGainCli.Domain.Entities;
+using System.Collections.Generic;
+
+namespace CapitalGainCli.Domain.Services
+{
+    public static class TaxCalculationService
+    {
+        private const decimal TaxCollectionMinimumThreshold = 20000;
+
+        public static List<TaxCalculationResult> CalculateTaxes(List<FinancialOperation> financialOperations)
+        {
+            return new List<TaxCalculationResult>();
+        }
+    }
+}
