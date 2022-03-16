5ef982cf9725ef5e36b1bf28b122e0fff64ce749
Antonio Felipe Melo Silva <antonio.melo@stone.com.br>
2022-03-15 22:00:26 -0300

    implement weighted average

--- src/CapitalGainCli.Domain/Services/TaxCalculationService.cs
+++ src/CapitalGainCli.Domain/Services/TaxCalculationService.cs
@@ -1,5 +1,7 @@
 ﻿using CapitalGainCli.Domain.Entities;
+using CapitalGainCli.Domain.Enums;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CapitalGainCli.Domain.Services
 {
@@ -9,7 +11,33 @@ namespace CapitalGainCli.Domain.Services
 
         public static List<TaxCalculationResult> CalculateTaxes(List<FinancialOperation> financialOperations)
         {
+            var resultList = new List<TaxCalculationResult>();
+            var averageOperationValue = CalculateAssetWeightedAverageValue(financialOperations.Where(x => x.Operation == OperationType.Buy));
+
+            foreach(var operation in financialOperations)
+            {
+                if(operation.Operation == OperationType.Buy || operation.UnitCost <= averageOperationValue)
+                {
+                    resultList.Add(new TaxCalculationResult { Tax = 0 });
+                }
+            }
+
             return new List<TaxCalculationResult>();
+        }
+
+        private static decimal CalculateAssetWeightedAverageValue(IEnumerable<FinancialOperation> financialOperations)
+        {
+            var operationCosts = financialOperations.Select(x => x.UnitCost).Distinct();
+            decimal numerator = 0;
+            decimal denominator = financialOperations.Count();
+
+            foreach (var cost in operationCosts)
+            {
+                var operations = financialOperations.Where(x => x.UnitCost == cost).Count();
+                numerator += operations * cost;
+            }
+
+            return numerator / denominator;
         }
     }
 }
